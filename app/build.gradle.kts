1040f5b3e35723729187517f92fb51c98bc87596
futureisAJASU <junyoung090928@gmail.com>
2026-08-04 03:09:17 +0900

    release: bump versionCode to 10007 for Beta Stable R5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.projectnuke.fusion"
         minSdk = 32
         targetSdk = 36
-versionCode = 10006
+versionCode = 10007
         versionName = "1.0.0-beta-stable"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
